72c733f6760cbad47b545928a264a642b6296d20
Mariner <57468305+Mariner10@users.noreply.github.com>
2023-03-27 22:14:45 -0400

    added Server stuff

--- constants.py
+++ constants.py
@@ -1,7 +1,16 @@
 #Throw those credentials in here!
 
+#What type of device are you using?
+#   (windows or macos) (MACOS ALSO MEANS LINUX)
+
+deviceType = "macos"
+
 life360_username = ""
 life360_password = ""
 
-#Your timezone
 timeZone = 'America/New_York'
+
+remote_logs_directory = ""
+local_logs_directory = ""
+
+serverHostname,serverPort,serverUser,serverPass = '',int(),'',''
